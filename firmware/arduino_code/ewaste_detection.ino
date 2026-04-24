928ebc891a8189c567ebf52308c3be3f165c8c3e
LuciferBot05 <gambre.2020@gmail.com>
2026-04-24 12:17:39 +0530

    Update ewaste_detection.ino

--- firmware/arduino_code/ewaste_detection.ino
+++ firmware/arduino_code/ewaste_detection.ino
@@ -1,1 +1,30 @@
+#include <Servo.h>
 
+Servo sorter;
+
+int sensorPin = 7;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  sorter.attach(9);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int objectDetected = digitalRead(sensorPin);
+
+  if(objectDetected == LOW)
+  {
+    Serial.println("Object detected");
+
+    delay(2000);
+
+    sorter.write(90);  
+    delay(2000);
+
+    sorter.write(0);  
+    delay(2000);
+  }
+
+}
