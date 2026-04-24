cde3a13258da5a3434404e94e2588f5f2eac8d0d
LuciferBot05 <gambre.2020@gmail.com>
2026-04-24 12:33:15 +0530

    Update sensor_test.ino

--- firmware/sensor_test/sensor_test.ino
+++ firmware/sensor_test/sensor_test.ino
@@ -1,1 +1,19 @@
+int sensorPin = 7;   // IR sensor OUT connected to pin 7
 
+void setup() {
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int sensorValue = digitalRead(sensorPin);
+
+  if (sensorValue == LOW) {
+    Serial.println("Object Detected");
+  } else {
+    Serial.println("No Object");
+  }
+
+  delay(500); // delay for stability
+}
